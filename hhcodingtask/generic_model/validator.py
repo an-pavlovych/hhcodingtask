62d0d297fe0efbb45a680a9165c2a4f73401d93c
an.pavlovych <an.pavlovych@gmail.com>
2018-04-27 11:32:44 +0300

    added errors parser

---
+++ hhcodingtask/generic_model/validator.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import ValidationError
+
+from cerberus import Validator
+
+
+def validate_data(scheme, data):
+    validator = Validator(scheme)
+    if not validator.validate(data):
+        return validator.errors
+
+
+def errors_to_validation_error(error_list):
+    django_errors = []
+    for field_name in error_list.keys():
+        for error_string in error_list[field_name]:
+            django_errors.append(ValidationError(error_string))
+    return django_errors
