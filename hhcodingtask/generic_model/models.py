3b5db5f8713464651bfa180b7b005a930f406202
an.pavlovych <an.pavlovych@gmail.com>
2018-04-27 11:32:19 +0300

    added default value for scheme. Fixed data column name

--- hhcodingtask/generic_model/models.py
+++ hhcodingtask/generic_model/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.conf import settings
+
+from .validator import validate_data
 
 
 class GenericModel(models.Model):
     title = models.CharField(max_length=32, null=False)
-    data = JSONField()
-    scheme = JSONField()
+    _data = JSONField(null=True)
+    scheme = JSONField(default=settings.SCHEME)
 
     @property
     def data(self):
-        return self.data
+        return self._data
 
     @data.setter
     def data(self, value):
-        pass
+        errors = validate_data(self.scheme, value)
+        if errors is None:
+            self._data = value
+        else:
+            raise ValueError('Could not set to new value. Reasons: {}'.format(errors))
 
     def __str__(self):
         return self.title
